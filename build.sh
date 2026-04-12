f6b904350365205a31063c494cef178ecae13646
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-12 12:35:30 +0200

    feat(docker): introduce Java 25 Docker image and refactor build structure
    - Added `java25/Dockerfile` for Java 25 environment (`eclipse-temurin:25-jre-noble`).
    - Introduced `latest/Dockerfile` as the new default build target.
    - Updated `build.sh` and `run.sh` scripts to align with the new directory structure and add Java 25 support.
    - Adjusted environment variable defaults for Fabric Loader and Installer versions.

--- build.sh
+++ build.sh
@@ -3,17 +3,23 @@
 docker buildx build \
   --platform linux/amd64,linux/arm64/v8,linux/riscv64,linux/ppc64le,linux/s390x \
   -t diareuse/minecraft:fabric-java17 \
-  -f Dockerfile.java17 \
+  -f java17/Dockerfile \
   --push .
 
 docker buildx build \
   --platform linux/amd64,linux/arm64/v8,linux/riscv64,linux/ppc64le,linux/s390x \
   -t diareuse/minecraft:fabric-java21 \
-  -f Dockerfile.java21 \
+  -f java21/Dockerfile \
+  --push .
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64/v8,linux/riscv64,linux/ppc64le,linux/s390x \
+  -t diareuse/minecraft:fabric-java25 \
+  -f java25/Dockerfile \
   --push .
 
 docker buildx build \
   --platform linux/amd64,linux/arm64/v8,linux/riscv64,linux/ppc64le,linux/s390x \
   -t diareuse/minecraft:fabric \
-  -f Dockerfile \
+  -f latest/Dockerfile \
   --push .
--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # build all archs
 #docker buildx build --platform linux/amd64,linux/arm64/v8,linux/riscv64,linux/ppc64le,linux/s390x -t diareuse/minecraft:fabric --push .
 
-docker build -t diareuse/minecraft:fabric .
+docker build -t diareuse/minecraft:fabric -f latest/Dockerfile .
 docker run --name "minecraft-fabric" --rm \
-  -e MINECRAFT="1.21.10" -e FABRIC_LOADER="0.17.3" -e FABRIC_INSTALLER="1.1.0" -p 25565:25565 \
+  -e MINECRAFT="1.21.10" -e FABRIC_LOADER="0.19.1" -e FABRIC_INSTALLER="1.1.1" -p 25565:25565 \
   -e PUID=501 -e PGID=20 \
   -v ./minecraft:/opt/minecraft \
   diareuse/minecraft:fabric
